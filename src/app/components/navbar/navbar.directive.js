623147f5731d98debe41d5f731d005633536dfb2
Trakan <trakan@trakan.com>
2018-01-08 21:18:17 -0600

    eslint cleanup

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -14,7 +14,7 @@ export function NavbarDirective() {
 }
 
 class NavbarController {
-  constructor ($scope, $rootScope, $window, ConfigService) {
+  constructor ($scope, $rootScope, $document, $window, ConfigService) {
     'ngInject';
 
     this.navMenuOptions = ConfigService.MENU_OPTS.nav;
@@ -25,23 +25,17 @@ class NavbarController {
     /** EVENT HANDLING **/
 
     let initMenu = $rootScope.$on('initMenu', (event, args) => {
-
       // scroll to top on route change
-      let scrollThreshold = document.querySelector(menuShrinkTrigger).offsetHeight;
-
+      let scrollThreshold = $document[0].querySelectorAll(menuShrinkTrigger)[0].offsetHeight;
       //if user already scrolled past point where menu shrunk, keep it shrunk when scroll back to the top
       let scrollToPosY = ( args.curScrollPos >= scrollThreshold ) ? scrollThreshold : 0;
       $window.scrollTo(0, scrollToPosY); //todo - animate to make this smoother
-
     });
 
-
-
     // shrink/fix navbar on scroll position, using jqlite
-    // FIXME - how to use $document to accomplish this, instead of document
-    let navMenuElement = angular.element(document.querySelector('.navbar'));
+    let navMenuElement = angular.element($document[0].querySelectorAll('.navbar')[0]);
     let fixedNavClass = 'affix-nav';
-    let scrollThreshold = document.querySelector(menuShrinkTrigger).offsetHeight;
+    let scrollThreshold = $document[0].querySelectorAll(menuShrinkTrigger)[0].offsetHeight;
 
     angular.element($window).bind('scroll', () => {
       if ( ($window.scrollY > scrollThreshold) && (!navMenuElement.hasClass(fixedNavClass)) ) {
